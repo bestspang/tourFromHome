e901732b79e23f07c6db301d22a29902476607c7
Kongphop <bestspang@gmail.com>
2020-05-15 01:41:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -197,11 +197,11 @@ def dashboard():
 def login():
     return render_template('login.html')
 
-@app.route("/chat")
-def login():
+@app.route('/chat')
+def chat():
     return render_template('main/chat_test.html')
 
-def messageReceived(methods=['GET', 'POST']):
+def messageReceived('/chat', methods=['GET', 'POST']):
     print('message was received!!!')
 
 @app.route('/main/profile', methods=['GET', 'POST'])
